48fc41fbf29e8792950c432edaedbcca90b9f2e2
hfantini <henrique.fantini@hotmail.com>
2025-12-31 08:44:51 -0300

    feat: create log macros

---
+++ src/log/macros.rs
@@ -0,0 +1,41 @@
+#[macro_export]
+macro_rules! trace {
+    ($($arg:tt)*) => {
+        $crate::log::logger::log_trace(format!($($arg)*));
+    };
+}
+
+#[macro_export]
+macro_rules! debug {
+    ($($arg:tt)*) => {
+        $crate::log::logger::log_debug(format!($($arg)*));
+    };
+}
+
+#[macro_export]
+macro_rules! info {
+    ($($arg:tt)*) => {
+        $crate::log::logger::log_info(format!($($arg)*));
+    };
+}
+
+#[macro_export]
+macro_rules! warn {
+    ($($arg:tt)*) => {
+        $crate::log::logger::log_warn(format!($($arg)*));
+    };
+}
+
+#[macro_export]
+macro_rules! error {
+    ($($arg:tt)*) => {
+        $crate::log::logger::log_error(format!($($arg)*));
+    };
+}
+
+#[macro_export]
+macro_rules! critical {
+    ($($arg:tt)*) => {
+        $crate::log::logger::log_critical(format!($($arg)*));
+    };
+}
